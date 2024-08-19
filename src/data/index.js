7f161ff3e42d3a9558616cdf32a23a0eb0f250e5
mxTech1996 <168294592+mxTech1996@users.noreply.github.com>
2024-08-19 14:32:06 -0600

    Add src/data/index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 
-export const pageName = "YimSports";
+export const pageName = "Yim Sports";
 export const primaryColor = "#000000";
 export const backgroundColor = "#FFFFFF";
 export const makeClearBackground = (color) => {
